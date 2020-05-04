420519ec7538bed0d4b6400faf123cd1fcc496a1
Q <q@misell.cymru>
2020-05-04 18:46:15 +0100

    add django-mathfilters

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 VERSION=$(sentry-cli releases propose-version || exit)
 
-docker build -t "as207960/hexdns-django:$VERSION" . || exit
-docker push "as207960/hexdns-django:$VERSION" || exit
+docker build -t "as207960/billing-django:$VERSION" . || exit
+docker push "as207960/billing-django:$VERSION" || exit
 
 sentry-cli releases --org as207960 new -p hexdns "$VERSION" || exit
 sentry-cli releases --org as207960 set-commits --auto "$VERSION"
