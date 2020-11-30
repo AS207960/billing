1c7317578e33366e51e5e305e466af1d99598b35
Q <q@misell.cymru>
2020-11-30 22:53:41 +0000

    fix migrations take 4

--- billing/migrations/0008_auto_20201123_0959.py
+++ billing/migrations/0008_auto_20201123_0959.py
@@ -26,6 +26,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('method_id', models.CharField(max_length=255)),
                 ('account', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='billing.Account')),
+                ('country_code', models.CharField(default='', max_length=2, validators=[django.core.validators.MinLengthValidator(2)]))
             ],
         ),
         migrations.CreateModel(
--- billing/migrations/0011_accountbillingaddress_default.py
+++ billing/migrations/0011_accountbillingaddress_default.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('billing', '0010_auto_20201123_1329'),
+        ('billing', '0009_auto_20201123_1048'),
     ]
 
     operations = [
