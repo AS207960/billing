bc3b4110b59e2553b8c79813f00b6806fcaed7b4
Q <q@misell.cymru>
2020-12-30 22:41:39 +0000

    fix fail old

--- billing/management/commands/fail-old.py
+++ billing/management/commands/fail-old.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 import datetime
 from django.utils import timezone
 from billing import models
+import django.core.exceptions
 
 FAIL_TIME = datetime.timedelta(days=1)
 
@@ -11,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = timezone.now()
-        fail_threshold = now + FAIL_TIME
+        fail_threshold = now - FAIL_TIME
 
         for ledger_item in models.LedgerItem.objects.filter(
             type=models.LedgerItem.TYPE_CHARGE,
@@ -20,3 +21,12 @@ class Command(BaseCommand):
         ):
             ledger_item.state = models.LedgerItem.STATE_FAILED
             ledger_item.save()
+
+            try:
+                charge_state = ledger_item.charge_state
+            except django.core.exceptions.ObjectDoesNotExist:
+                charge_state = None
+
+            if charge_state and not charge_state.last_error:
+                charge_state.last_error = "Payment timed out"
+                charge_state.save()
