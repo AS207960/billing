55e90e47bc72e43afc2a7b206ef10f34d499e5ba
Q <q@misell.cymru>
2021-03-03 11:41:03 +0000

    change exchange rate source to ECB

--- billing/management/commands/sync-exchange.py
+++ billing/management/commands/sync-exchange.py
@@ -1,36 +1,47 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
+import xml.etree.ElementTree
 import requests
 import datetime
 import pytz
-import json
 import decimal
 from billing import models
 
 
 class Command(BaseCommand):
-    help = 'Syncs exchange rates'
+    help = 'Syncs exchange rates from ECB'
 
     def handle(self, *args, **options):
-        r = requests.get(f"https://openexchangerates.org/api/latest.json?app_id={settings.OPEN_EXCHANGE_API_KEY}")
+        r = requests.get(f"https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
         if r.status_code != 200:
             raise CommandError(f"Error getting exchange rates: {r.text}")
         try:
-            d = json.loads(r.text)
-        except json.JSONDecodeError as e:
+            d = xml.etree.ElementTree.fromstring(r.text)
+        except xml.etree.ElementTree.ParseError as e:
             raise CommandError(f"Error decoding json: {str(e)}")
 
-        timestamp = datetime.datetime.fromtimestamp(d["timestamp"], pytz.utc)
-        for currency, value in d["rates"].items():
-            existing_model = models.ExchangeRate.objects.filter(currency=currency).first()
-            if existing_model:
-                existing_model.timestamp = timestamp
-                existing_model.rate = decimal.Decimal(value)
-                existing_model.save()
-            else:
-                new_model = models.ExchangeRate(
-                    currency=currency,
-                    timestamp=timestamp,
-                    rate=decimal.Decimal(value)
-                )
-                new_model.save()
+        namespaces = {
+            "gesmes": "http://www.gesmes.org/xml/2002-08-01",
+            "eurofxref": "http://www.ecb.int/vocabulary/2002-08-01/eurofxref",
+        }
+
+        cube = d.find("eurofxref:Cube/eurofxref:Cube[@time]", namespaces=namespaces)
+        if not cube:
+            raise CommandError(f"Response did not contain Cube element")
+
+        timestamp = datetime.datetime.strptime(cube.attrib.get("time"), "%Y-%m-%d")
+        timestamp = timestamp.replace(tzinfo=pytz.utc, hour=0, minute=0, second=0)
+
+        rates = cube.findall("./eurofxref:Cube[@rate][@currency]", namespaces=namespaces)
+
+        currencies = [("EUR", decimal.Decimal("1.0"))]
+
+        for rate in rates:
+            currency = rate.attrib.get("currency")
+            fx_rate = rate.attrib.get("rate")
+            currencies.append((currency, decimal.Decimal(fx_rate)))
+
+        for currency, value in currencies:
+            models.ExchangeRate.objects.update_or_create(currency=currency, defaults={
+                "timestamp": timestamp,
+                "rate": value
+            })
